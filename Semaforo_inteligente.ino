4f8dcaebcc4ea03a654d4638a8fc6a2cb2293656
tomashonse <85421857+tomashonse@users.noreply.github.com>
2022-02-07 15:51:58 -0300

    Update Semaforo_inteligente.ino

--- Semaforo_inteligente.ino
+++ Semaforo_inteligente.ino
@@ -1,31 +1,29 @@
-// Nombro los pines para trabajar con mayor facilidad
+// Name the pins
 
-int LED_ROJO = 2;
-int LED_AMARILLO = 3;
-int LED_VERDE = 4;
-int BOTON = 7;
-
-// Inicializo los pines, ya que en este caso son todos digitales
+int LED_RED = 2;
+int LED_YELLOW = 3;
+int LED_GREEN = 4;
+int BUTTON = 7;
 
 void setup () {
-  pinMode(LED_ROJO, OUTPUT);
-  pinMode(LED_AMARILLO, OUTPUT);
-  pinMode(LED_VERDE, OUTPUT);
-  pinMode(BOTON, INPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_YELLOW, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(BUTTON, INPUT);
 }
 
 void loop (){
-  if(digitalRead(BOTON) == LOW) { // Si el botón no está presionado que se mantenga encendido el LED azul
-    digitalWrite(LED_VERDE, HIGH);
+  if(digitalRead(BUTTON) == LOW) { 
+    digitalWrite(LED_GREEN, HIGH);
   }
-  else if(digitalRead(BOTON) == HIGH) { // Si se presiona el botón inicializa la secuencia
-    delay(5000); // 1 segundo = 1000 milisegundos
-    digitalWrite(LED_VERDE, LOW);
-    digitalWrite(LED_AMARILLO, HIGH);
+  else if(digitalRead(BOTON) == HIGH) {
+    delay(5000); // 1s = 1000ms
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_YELLOW, HIGH);
     delay(5000);
-    digitalWrite(LED_AMARILLO, LOW);
-    digitalWrite(LED_ROJO, HIGH);
-    delay(6000);
-    digitalWrite(LED_ROJO, LOW);
+    digitalWrite(LED_YELLOW, LOW);
+    digitalWrite(LED_RED, HIGH);
+    delay(60000);
+    digitalWrite(LED_RED, LOW);
   }
 }
